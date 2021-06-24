333ba044c6b40a1b2361eeee42d8cae5feeb43e8
Karol <karol@dev.praetorian.technology>
2021-06-24 18:34:59 +0200

    Added behat/behat dependency. Implemented 11 test scenarios (task: AS-130)

---
+++ features/cache.feature
@@ -0,0 +1,68 @@
+Feature: Cache service
+  In order to prove that the RedisCacheService is working properly
+  As a developer, a RedisCacheService user
+  I need to be able to set, get, tag, delete, etc. cached values
+
+  Scenario: It sets and gets saved value from cache
+    Given the redis cache instance does not contain any value under the key "example_key"
+    When I add the "example_value" under the "example_key" to the cache
+    Then I should have "example_value" under the "example_key" in the cache
+
+  Scenario: It gets existing value from cache
+    Given the redis cache instance contains "example_value" under the key "example_key"
+    Then I should have "example_value" under the "example_key" in the cache
+
+  Scenario: It tries to get non-existing value from cache
+    Given the redis cache instance does not contain any value under the key "example_key"
+    Then I should not have any value under the "example_key" in the cache
+
+  Scenario: It deletes value from cache
+    Given the redis cache instance contains "example_value" under the key "example_key"
+    When I delete value under the "example_key" from the cache
+    Then I should not have any value under the "example_key" in the cache
+
+  Scenario: It sets and tags value in cache
+    Given the redis cache instance does not contain any value under the key "example_key"
+    When I add the "example_value" under the "example_key" tagged with "example_tag" to the cache
+    Then I should have "example_value" under the "example_key" in the cache
+    And I should have "example_value" tagged by the "example_tag" under the "example_key" in the cache
+
+  Scenario: It tags existing value in cache
+    Given the redis cache instance contains "example_value" under the key "example_key" which is not tagged by "example_tag"
+    When I tag the "example_key" with "example_tag"
+    Then I should have "example_value" tagged by the "example_tag" under the "example_key" in the cache
+
+  Scenario: It untags existing value in cache
+    Given the redis cache instance contains "example_value" under the key "example_key" which is tagged by "example_tag"
+    When I untag the "example_key" with "example_tag"
+    Then I should have "example_value" under the "example_key" in the cache
+    And I should not have "example_value" tagged by the "example_tag" under the "example_key" in the cache
+
+  Scenario: It tags non-existing key in cache
+    Given the redis cache instance does not contain any value under the key "example_key"
+    When I tag the "example_key" with "example_tag"
+    Then I should not have key "example_key" tagged by the "example_tag" in the cache
+    And I should not have any value under the "example_key" in the cache
+
+  Scenario: It deletes tagged key from cache
+    Given the redis cache instance contains "example_value" under the key "example_key" which is tagged by "example_tag"
+    When I delete value under the "example_key" from the cache
+    Then I should not have any value under the "example_key" in the cache
+    And I should not have key "example_key" tagged by the "example_tag" in the cache
+
+  Scenario: It clears by tag from cache
+    Given the redis cache instance is clean
+    Given the redis cache instance contains "example_value" under the key "example_key" which is tagged by "example_tag"
+    Given the redis cache instance contains "example_value_2" under the key "example_key_2" which is tagged by "example_tag"
+    When I clear by tag "example_tag" from cache
+    Then I should not have any value under the "example_key" in the cache
+    And I should not have any value under the "example_key_2" in the cache
+    And I should not have any key tagged by the "example_tag" in the cache
+
+  Scenario: It gets tagged from cache
+    Given the redis cache instance is clean
+    Given the redis cache instance contains "example_value" under the key "example_key" which is tagged by "example_tag"
+    Given the redis cache instance contains "example_value_2" under the key "example_key_2" which is tagged by "example_tag"
+    Then I should have "example_value" tagged by the "example_tag" under the "example_key" in the cache
+    And I should have "example_value" tagged by the "example_tag" under the "example_key" in the cache
+    And I should have exactly 2 keys tagged by the "example_tag" in the cache
---
+++ tests/Behat/RedisCacheServiceContext.php
@@ -0,0 +1,200 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Praetorian\Tests\CacheService\Behat;
+
+use Behat\Behat\Context\Context;
+use Praetorian\CacheService\RedisCacheService;
+
+final class RedisCacheServiceContext implements Context
+{
+    private RedisCacheService $redisCacheService;
+
+    public function __construct(string $host, ?int $port = null)
+    {
+        $this->redisCacheService = new RedisCacheService($host, $port);
+    }
+
+    /**
+     * @Given the redis cache instance does not contain any value under the key :key
+     */
+    public function theRedisCacheInstanceDoesNotContainAnyValueUnderTheKey(string $key)
+    {
+        $this->redisCacheService->delete($key);
+    }
+
+    /**
+     * @Given the redis cache instance contains :value under the key :key
+     */
+    public function theRedisCacheInstanceContainsUnderTheKey(mixed $value, string $key)
+    {
+        $this->redisCacheService->set($key, $value);
+    }
+
+    /**
+     * @Given the redis cache instance contains :value under the key :key which is not tagged by :tag
+     */
+    public function theRedisCacheInstanceContainsUnderTheKeyWhichIsNotTaggedBy(mixed $value, string $key, string $tag)
+    {
+        $this->redisCacheService->set($key, $value);
+        //to avoid case when previously existing value under this key was tagged by this tag
+        $this->redisCacheService->untag($key, $tag);
+    }
+
+    /**
+     * @Given the redis cache instance contains :value under the key :key which is tagged by :tag
+     */
+    public function theRedisCacheInstanceContainsUnderTheKeyWhichIsTaggedBy(mixed $value, string $key, string $tag)
+    {
+        $this->redisCacheService->set($key, $value, $tag);
+    }
+
+    /**
+     * @Given the redis cache instance is clean
+     */
+    public function theRedisCacheInstanceIsClean()
+    {
+        $this->redisCacheService->clear();
+    }
+
+    /**
+     * @When I add the :value under the :key to the cache
+     */
+    public function iAddTheUnderTheToTheCache(mixed $value, string $key)
+    {
+        $this->redisCacheService->set($key, $value);
+    }
+
+    /**
+     * @Then I should have :value under the :key in the cache
+     */
+    public function iShouldHaveUnderTheInTheCache(mixed $value, string $key)
+    {
+        if ($this->redisCacheService->get($key) !== $value) {
+            throw new \RuntimeException(sprintf('Wrong value in cache. Received: %s. Expected: %s', $this->redisCacheService->get($key), $value));
+        }
+    }
+
+    /**
+     * @When I delete value under the :key from the cache
+     */
+    public function iDeleteValueUnderTheFromTheCache(string $key)
+    {
+        $this->redisCacheService->delete($key);
+    }
+
+    /**
+     * @Then I should not have any value under the :key in the cache
+     */
+    public function iShouldNotHaveAnyValueUnderTheInTheCache(string $key)
+    {
+        $value = $this->redisCacheService->get($key);
+
+        if ($value !== null) {
+            throw new \RuntimeException('Wrong value in cache. Expected null');
+        }
+    }
+
+    /**
+     * @When I add the :value under the :key tagged with :tag to the cache
+     */
+    public function iAddTheUnderTheTaggedWithToTheCache(mixed $value, string $key, string $tag)
+    {
+        $this->redisCacheService->set($key, $value, $tag);
+    }
+
+    /**
+     * @Then I should have :value tagged by the :tag under the :key in the cache
+     */
+    public function iShouldHaveTaggedByTheUnderTheInTheCache(mixed $value, string $tag, string $key)
+    {
+        $items = $this->redisCacheService->getTagged($tag);
+
+        foreach ($items as $itemKey => $itemValue) {
+            if ($itemKey === $key && $itemValue === $value) {
+                return;
+            }
+        }
+
+        throw new \RuntimeException(sprintf('Cache does not contain value %s tagged by %s', $value, $tag));
+    }
+
+    /**
+     * @Then I should not have :value tagged by the :tag under the :key in the cache
+     */
+    public function iShouldNotHaveTaggedByTheUnderTheInTheCache(mixed $value, string $tag, string $key)
+    {
+        $items = $this->redisCacheService->getTagged($tag);
+
+        foreach ($items as $itemKey => $itemValue) {
+            if ($itemKey === $key && $itemValue === $value) {
+                throw new \RuntimeException(sprintf('Cache contains value %s tagged by %s', $value, $tag));
+            }
+        }
+    }
+
+    /**
+     * @When I tag the :key with :tag
+     */
+    public function iTagTheWith(string $key, string $tag)
+    {
+        $this->redisCacheService->tag($key, $tag);
+    }
+
+    /**
+     * @When I untag the :key with :tag
+     */
+    public function iUntagTheWith(string $key, string $tag)
+    {
+        $this->redisCacheService->untag($key, $tag);
+    }
+
+    /**
+     * @Then I should not have key :key tagged by the :tag in the cache
+     */
+    public function iShouldNotHaveKeyTaggedByTheInTheCache(string $key, string $tag)
+    {
+        $items = $this->redisCacheService->getTagged($tag);
+
+        foreach ($items as $itemKey => $itemValue) {
+            if ($itemKey === $key) {
+                throw new \RuntimeException(sprintf('Cache contains key %s tagged by %s', $key, $tag));
+            }
+        }
+    }
+
+    /**
+     * @Then I should not have any key tagged by the :tag in the cache
+     */
+    public function iShouldNotHaveAnyKeyTaggedByTheInTheCache(string $tag)
+    {
+        $items = $this->redisCacheService->getTagged($tag);
+
+        foreach ($items as $itemKey => $itemValue) {
+            throw new \RuntimeException(sprintf('Cache contains key %s tagged by %s', $key, $tag));
+        }
+    }
+
+    /**
+     * @When I clear by tag :tag from cache
+     */
+    public function iClearByTagFromCache(string $tag)
+    {
+        $this->redisCacheService->clearByTag($tag);
+    }
+
+    /**
+     * @Then I should have exactly :n keys tagged by the :tag in the cache
+     */
+    public function iShouldHaveExactlyKeysTaggedByTheInTheCache(int $n, string $tag)
+    {
+        $items = $this->redisCacheService->getTagged($tag);
+
+        $numberOfitems = count(iterator_to_array($items));
+
+        if ($numberOfitems !== $n) {
+            throw new \RuntimeException(sprintf('Cache contains %d keys (expected %d) tagged by %s', $numberOfitems, $n, $tag));
+        }
+    }
+}
